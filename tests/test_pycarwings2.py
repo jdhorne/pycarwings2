7e530e74f9d28d4c38edd53b784c0634aae65574
Phil Cole <filcole@gmail.com>
2019-02-03 22:33:07 +0000

    Add tests for battery_percent

--- tests/test_pycarwings2.py
+++ tests/test_pycarwings2.py
@@ -5,7 +5,10 @@ import logging
 import sys
 import pytest
 from pycarwings2 import Session, CarwingsError
-from pycarwings2.responses import CarwingsLatestClimateControlStatusResponse
+from pycarwings2.responses import (
+    CarwingsLatestBatteryStatusResponse,
+    CarwingsLatestClimateControlStatusResponse
+)
 
 logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
 
@@ -48,7 +51,20 @@ def test_get_latest_hvac_status_on():
 def test_get_latest_hvac_status_off():
     # Source: From 24kWh Leaf (Climate is OFF)
     climateresponse = """
-{"status":200,"RemoteACRecords":{"OperationResult":"START_BATTERY","OperationDateAndTime":"11-Jan-2019 17:51","RemoteACOperation":"STOP","ACStartStopDateAndTime":"2019/01/11 16:52","CruisingRangeAcOn":"74328.0","CruisingRangeAcOff":"79544.0","ACStartStopURL":"","PluginState":"NOT_CONNECTED","ACDurationBatterySec":"900","ACDurationPluggedSec":"7200"}}
+{
+  "status":200,
+  "RemoteACRecords":{
+    "OperationResult":"START_BATTERY",
+    "OperationDateAndTime":"11-Jan-2019 17:51",
+    "RemoteACOperation":"STOP",
+    "ACStartStopDateAndTime":"2019/01/11 16:52",
+    "CruisingRangeAcOn":"74328.0",
+    "CruisingRangeAcOff":"79544.0",
+    "ACStartStopURL":"",
+    "PluginState":"NOT_CONNECTED",
+    "ACDurationBatterySec":"900",
+    "ACDurationPluggedSec":"7200"}
+}
 """
     response = json.loads(climateresponse)
     status = CarwingsLatestClimateControlStatusResponse(response)
@@ -80,7 +96,8 @@ def test_get_latest_hvac_status_error():
         CarwingsLatestClimateControlStatusResponse(response)
         assert 'Should have exception here'
 
-    assert 'could not establish communications with vehicle' in str(excinfo.value)
+    assert 'could not establish communications with vehicle' in str(
+        excinfo.value)
 
 
 def test_get_latest_hvac_status_empty():
@@ -92,3 +109,159 @@ def test_get_latest_hvac_status_empty():
     response = json.loads(climateresponse)
     status = CarwingsLatestClimateControlStatusResponse(response)
     assert not status.is_hvac_running
+
+
+def test_get_latest_battery_status_24kWh_30percent_0degredation():
+    # not connected to a charger
+    batteryresponse = """
+        {
+            "status":200,
+            "message":"success",
+            "BatteryStatusRecords":{
+                "OperationResult":"START",
+                "OperationDateAndTime":"Feb  9, 2016 11:09 PM",
+                "BatteryStatus":{
+                    "BatteryChargingStatus":"NOT_CHARGING",
+                    "BatteryCapacity":"12",
+                    "BatteryRemainingAmount":"3",
+                    "BatteryRemainingAmountWH":"",
+                    "BatteryRemainingAmountkWH":""
+                },
+                "PluginState":"NOT_CONNECTED",
+                "CruisingRangeAcOn":"39192.0",
+                "CruisingRangeAcOff":"39744.0",
+                "TimeRequiredToFull":{
+                    "HourRequiredToFull":"18",
+                    "MinutesRequiredToFull":"30"
+                },
+                "TimeRequiredToFull200":{
+                    "HourRequiredToFull":"6",
+                    "MinutesRequiredToFull":"0"
+                },
+                "TimeRequiredToFull200_6kW":{
+                    "HourRequiredToFull":"4",
+                    "MinutesRequiredToFull":"0"
+                },
+                "NotificationDateAndTime":"2016/02/10 04:10",
+                "TargetDate":"2016/02/10 04:09"
+            }
+        }
+"""
+    response = json.loads(batteryresponse)
+    status = CarwingsLatestBatteryStatusResponse(response)
+    assert status.battery_percent == 100 * 3 / 12
+
+
+def test_get_latest_battery_status_24kWh_50percent_1degredation():
+    # Charging, but with 50% charged, with 11 bars max charge (1 bar lost)
+    batteryresponse = """
+{
+  "status": 200,
+  "BatteryStatusRecords": {
+    "OperationResult": "START",
+    "OperationDateAndTime": "26-Jan-2019 22:22",
+    "BatteryStatus": {
+      "BatteryChargingStatus": "NORMAL_CHARGING",
+      "BatteryCapacity": "11",
+      "BatteryRemainingAmount": "6",
+      "BatteryRemainingAmountWH": "",
+      "BatteryRemainingAmountkWH": ""
+    },
+    "PluginState": "CONNECTED",
+    "CruisingRangeAcOn": "66024.0",
+    "CruisingRangeAcOff": "69168.0",
+    "TimeRequiredToFull200_6kW": {
+      "HourRequiredToFull": "3",
+      "MinutesRequiredToFull": "0"
+    },
+    "NotificationDateAndTime": "2019/01/26 21:22",
+    "TargetDate": "2019/01/26 21:22"
+  }
+}
+"""
+    response = json.loads(batteryresponse)
+    status = CarwingsLatestBatteryStatusResponse(response)
+    assert status.battery_percent == 100 * 6 / 12
+
+
+def test_get_latest_battery_status_24kWh_91percent_1degredation():
+    # Not charging, but with 11 bars charged, 11 bars max charge (1 bar lost)
+    batteryresponse = """
+{
+  "status": 200,
+  "BatteryStatusRecords": {
+    "OperationResult": "START",
+    "OperationDateAndTime": "09-Jan-2019 20:45",
+    "BatteryStatus": {
+      "BatteryChargingStatus": "NOT_CHARGING",
+      "BatteryCapacity": "11",
+      "BatteryRemainingAmount": "11",
+      "BatteryRemainingAmountWH": "",
+      "BatteryRemainingAmountkWH": ""
+    },
+    "PluginState": "NOT_CONNECTED",
+    "CruisingRangeAcOn": "97704.0",
+    "CruisingRangeAcOff": "105984.0",
+    "TimeRequiredToFull": {
+      "HourRequiredToFull": "3",
+      "MinutesRequiredToFull": "30"
+    },
+    "TimeRequiredToFull200": {
+      "HourRequiredToFull": "2",
+      "MinutesRequiredToFull": "30"
+    },
+    "TimeRequiredToFull200_6kW": {
+      "HourRequiredToFull": "2",
+      "MinutesRequiredToFull": "0"
+    },
+    "NotificationDateAndTime": "2019/01/09 19:45",
+    "TargetDate": "2019/01/09 19:45"
+  }
+}
+"""
+    response = json.loads(batteryresponse)
+    status = CarwingsLatestBatteryStatusResponse(response)
+    assert status.battery_percent == 100 * 11 / 12
+
+
+def test_get_latest_battery_status_30kWh_91percent_0degredation():
+    # not connected to a charger - as at 21/01/2019 20:01 (for a 30kWh leaf)
+    batteryresponse = """
+        {
+            "status":200,
+            "BatteryStatusRecords": {
+                "OperationResult":"START",
+                "OperationDateAndTime":"21-Jan-2019 13:29",
+                "BatteryStatus":{
+                    "BatteryChargingStatus":"NOT_CHARGING",
+                    "BatteryCapacity":"240",
+                    "BatteryRemainingAmount":"220",
+                    "BatteryRemainingAmountWH":"24480",
+                    "BatteryRemainingAmountkWH":"",
+                    "SOC":{
+                        "Value":"91"
+                    }
+                },
+                "PluginState":"NOT_CONNECTED",
+                "CruisingRangeAcOn":"146000",
+                "CruisingRangeAcOff":"168000",
+                "TimeRequiredToFull":{
+                    "HourRequiredToFull":"4",
+                    "MinutesRequiredToFull":"30"
+                },
+                "TimeRequiredToFull200":{
+                    "HourRequiredToFull":"3"
+                    ,"MinutesRequiredToFull":"0"
+                },
+                "TimeRequiredToFull200_6kW":{
+                    "HourRequiredToFull":"1",
+                    "MinutesRequiredToFull":"30"
+                },
+                "NotificationDateAndTime":"2019/01/21 13:29",
+                "TargetDate":"2019/01/21 13:29"
+            }
+        }
+"""
+    response = json.loads(batteryresponse)
+    status = CarwingsLatestBatteryStatusResponse(response)
+    assert status.battery_percent == 91
